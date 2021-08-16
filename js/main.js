6873edc14377aacdf61bc3bbfe42811a5c31b952
thiagoviks <thiagosudgomes@gmail.com>
2021-08-16 13:35:42 -0300

    Adicionado lógica de movimentação

--- js/main.js
+++ js/main.js
@@ -38,6 +38,7 @@ movefundo();
 movejogador();
 moveinimigo1();
 moveinimigo2();
+moveamigo();
 
 } // Fim da função loop()
 
@@ -86,6 +87,30 @@ function movefundo() {
                     
                 }
         } //Fim da função moveinimigo1()
+
+        function moveinimigo2() {
+            posicaoX = parseInt($("#inimigo2").css("left"));
+        $("#inimigo2").css("left",posicaoX-3);
+                    
+            if (posicaoX<=0) {
+                
+            $("#inimigo2").css("left",775);
+                        
+            }
+    } // Fim da função moveinimigo2()
+    
+    function moveamigo() {
+	
+        posicaoX = parseInt($("#amigo").css("left"));
+        $("#amigo").css("left",posicaoX+1);
+                    
+            if (posicaoX>906) {
+                
+            $("#amigo").css("left",0);
+                        
+            }
+    
+    } // fim da função moveamigo()
     
 } // Fim da função start
 
